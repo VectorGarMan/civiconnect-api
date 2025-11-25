e5218312f35e5ca101491620014fa71a501c9197
VectorGarMan <vectorgarman@gmail.com>
2025-11-25 02:47:32 -0600

    Notes on how to kill port 8080 of java if its turned on

--- src/main/java/com/vectorgarman/civiconnect/CiviconnectApplication.java
+++ src/main/java/com/vectorgarman/civiconnect/CiviconnectApplication.java
@@ -3,6 +3,8 @@ package com.vectorgarman.civiconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// netstat -ano | findstr :8080
+// taskkill /PID ##### /F
 @SpringBootApplication
 public class CiviconnectApplication {
 	public static void main(String[] args) {
