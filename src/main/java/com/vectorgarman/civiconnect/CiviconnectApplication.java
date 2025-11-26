051f94ab81a7348727511f0011b18fdf83692b81
VectorGarMan <vectorgarman@gmail.com>
2025-11-25 22:05:59 -0600

    Comentario sobre url de swagger

--- src/main/java/com/vectorgarman/civiconnect/CiviconnectApplication.java
+++ src/main/java/com/vectorgarman/civiconnect/CiviconnectApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // netstat -ano | findstr :8080
 // taskkill /PID ##### /F
+
+// Swagger - http://localhost:8080/swagger-ui/index.html#
+
 @SpringBootApplication
 public class CiviconnectApplication {
 	public static void main(String[] args) {
